70a108ecd05032ce02c232ab1636f82046480743
jackparnell <jack@starsol.co.uk>
2020-12-04 11:31:35 +0000

    Link to branch view in review

---
+++ symfony/src/Controller/BranchController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Exception\NotFoundException;
+use App\Repository\BranchRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BranchController extends AbstractController
+{
+    private BranchRepository $branchRepository;
+
+    public function __construct(
+        BranchRepository $branchRepository
+    ) {
+        $this->branchRepository = $branchRepository;
+    }
+
+    /**
+     * @Route (
+     *     "/branch/{slug}",
+     *     name="branch-view-by-slug",
+     *     methods={"GET", "HEAD"}
+     * )
+     */
+    public function viewBySlug(string $slug): Response
+    {
+        try {
+            $branch = $this->branchRepository->findOnePublishedBySlug($slug);
+        } catch (NotFoundException $e) {
+            throw $this->createNotFoundException($e->getMessage());
+        }
+
+        return $this->render(
+            'branch/view.html.twig',
+            [
+                'agency' => $branch->getAgency(),
+                'branch' => $branch,
+            ]
+        );
+    }
+}
--- symfony/src/Repository/BranchRepository.php
+++ symfony/src/Repository/BranchRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Branch;
+use App\Exception\NotFoundException;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use function sprintf;
 
 /**
  * @method Branch|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,5 +19,21 @@ class BranchRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Branch::class);
+    }
+
+    public function findOnePublishedBySlug(string $slug): Branch
+    {
+        $branch = $this->findOneBy(
+            [
+                'slug' => $slug,
+                'published' => true,
+            ]
+        );
+
+        if (null === $branch) {
+            throw new NotFoundException(sprintf('No published branch with slug %s could be found.', $slug));
+        }
+
+        return $branch;
     }
 }
