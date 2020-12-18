643e990923e9b73ded6cf4d57631a4acc79ca337
jackparnell <jack@starsol.co.uk>
2020-12-18 11:32:57 +0000

    Property fixture latitudes and longitudes

--- symfony/src/DataFixtures/PropertyFixtures.php
+++ symfony/src/DataFixtures/PropertyFixtures.php
@@ -32,40 +32,52 @@ class PropertyFixtures extends Fixture
             ->setVendorPropertyId(self::PROPERTY_249_VENDOR_PROPERTY_ID)
             ->setAddressLine1('249 Victoria Road')
             ->setCity('Cambridge')
-            ->setPostcode('CB4 3LF');
+            ->setPostcode('CB4 3LF')
+            ->setLatitude(52.21507263)
+            ->setLongitude(0.11237954);
 
         $properties[] = (new Property())
             ->setVendorPropertyId(self::PROPERTY_25_VENDOR_PROPERTY_ID)
             ->setAddressLine1('25 Bateman Street')
             ->setCity('Cambridge')
-            ->setPostcode('CB2 1NB');
+            ->setPostcode('CB2 1NB')
+            ->setLatitude(52.19556427)
+            ->setLongitude(0.12813538);
 
         $properties[] = (new Property())
             ->setVendorPropertyId(self::PROPERTY_44_VENDOR_PROPERTY_ID)
             ->setAddressLine1('44 Fanshawe Road')
             ->setCity('Cambridge')
-            ->setPostcode('CB1 3QY');
+            ->setPostcode('CB1 3QY')
+            ->setLatitude(52.19140625)
+            ->setLongitude(0.14332861);
 
         $properties[] = (new Property())
             ->setVendorPropertyId(self::PROPERTY_22_VENDOR_PROPERTY_ID)
             ->setAddressLine1('22 Mingle Lane')
             ->setAddressLine2('Great Shelford')
             ->setCity('Cambridge')
-            ->setPostcode('CB22 5BG');
+            ->setPostcode('CB22 5BG')
+            ->setLatitude(52.14856600)
+            ->setLongitude(0.14268900);
 
         $properties[] = (new Property())
             ->setVendorPropertyId(self::PROPERTY_1_VENDOR_PROPERTY_ID)
             ->setAddressLine1('1 Primrose Lane')
             ->setAddressLine2('Waterbeach')
             ->setCity('Cambridge')
-            ->setPostcode('CB25 9JZ');
+            ->setPostcode('CB25 9JZ')
+            ->setLatitude(52.26825500)
+            ->setLongitude(0.18656500);
 
         $properties[] = (new Property())
             ->setVendorPropertyId(self::PROPERTY_17_VENDOR_PROPERTY_ID)
             ->setAddressLine1('17 Redgate Road')
             ->setAddressLine2('Girton')
             ->setCity('Cambridge')
-            ->setPostcode('CB3 0PP');
+            ->setPostcode('CB3 0PP')
+            ->setLatitude(52.23755646)
+            ->setLongitude(0.08636630);
 
         foreach ($properties as $property) {
             $property->setPublished(true);
