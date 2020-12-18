0238ee39a0ce4149fcca604b501c8caa8dfc59fe
jackparnell <jack@starsol.co.uk>
2020-12-18 20:00:53 +0000

    PropertyControllerTest

---
+++ symfony/tests/Controller/PropertyControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use function json_decode;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class PropertyControllerTest extends WebTestCase
+{
+    public function testViewBySlug()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/property/ccc5382816c1');
+
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+    }
+
+    public function testLookupSlugFromVendorId()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/property/lookup-slug-from-vendor-id?vendorPropertyId=ZmM5Yzc5MzMyODAyZTc4IDE3MDQ0OTcyIDMzZjhlNDFkNGU1MzY0Mw==');
+
+        $response = $client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+
+        $content = json_decode($response->getContent(), true);
+        $this->assertEquals('ccc5382816c1', $content['slug']);
+    }
+}
