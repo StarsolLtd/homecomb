98fbffd9f6003afe639612fefaead6b14aa90eee
jackparnell <jack@starsol.co.uk>
2020-12-29 20:12:20 +0000

    Agency Admin Home, partial

--- symfony/assets/js/pages/AgencyAdmin/Home.js
+++ symfony/assets/js/pages/AgencyAdmin/Home.js
@@ -1,6 +1,8 @@
 import React, {Fragment} from 'react';
 import {Container, Row, Col} from 'reactstrap';
 import DataLoader from "../../components/DataLoader";
+import ReactTable from 'react-table-v6'
+import 'react-table-v6/react-table.css'
 
 class Home extends React.Component {
     constructor(props) {
@@ -11,7 +13,6 @@ class Home extends React.Component {
             reviews: [],
             loaded: false,
         };
-
         this.loadData = this.loadData.bind(this);
     }
 
@@ -30,17 +31,31 @@ class Home extends React.Component {
                             </Col>
                         </Row>
 
-                        <Row>
-                            <Col md="12" className="bg-white rounded shadow-sm p-4 mb-4">
-                                <h2 className="mb-1">Your branches</h2>
-
+                        <Row className="bg-white rounded shadow-sm p-4 mb-4">
+                            <Col md="6" className="mb-4">
+                                <h2 className="mb-4">Reviews of {this.state.agency.name}</h2>
+                                <ReactTable
+                                    data={this.state.reviews}
+                                    columns={[
+                                        {Header: "Author", accessor: "author"},
+                                        {Header: "Property", accessor: "property.addressLine1"},
+                                    ]}
+                                    defaultPageSize={10}
+                                    showPageSizeOptions={false}
+                                    className="-striped -highlight"
+                                />
                             </Col>
-                        </Row>
-
-                        <Row>
-                            <Col md="12" className="bg-white rounded shadow-sm p-4 mb-4">
-                                <h2 className="mb-1">Your reviews</h2>
-
+                            <Col md="6" className="mb-4">
+                                <h2 className="mb-4">Branches of {this.state.agency.name}</h2>
+                                <ReactTable
+                                    data={this.state.branches}
+                                    columns={[
+                                        {Header: "Name", accessor: "name"},
+                                    ]}
+                                    defaultPageSize={10}
+                                    showPageSizeOptions={false}
+                                    className="-striped -highlight"
+                                />
                             </Col>
                         </Row>
                     </Fragment>
