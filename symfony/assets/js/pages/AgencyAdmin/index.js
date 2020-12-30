5b7dcea4c5fb01dfc3e34f243ddfc23c739782ba
jackparnell <jack@starsol.co.uk>
2020-12-30 10:40:26 +0000

    index.js

--- symfony/assets/js/pages/AgencyAdmin/index.js
+++ symfony/assets/js/pages/AgencyAdmin/index.js
@@ -1,13 +1,67 @@
-import React from 'react'
+import React from "react"
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from "react-router-dom"
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+
+import $ from 'jquery';
+import 'jquery-ui-bundle';
+import CreateAgency from "./CreateAgency";
+import UpdateAgency from "./UpdateAgency";
+import CreateReviewSolicitation from "../CreateReviewSolicitation";
+
+import AgencyAdminHome from "./Home";
+import LayoutFooter from "./LayoutFooter";
+import LayoutHeader from "./LayoutHeader";
+import LayoutSidebar from "./LayoutSidebar";
+import {Col, Row} from "reactstrap";
 
 import '../../../styles/app.scss';
-import Router from "./Router";
 
-ReactDOM.render(
-    <BrowserRouter>
-        <Router />
-    </BrowserRouter>,
-    document.getElementById('root')
-);
+class Index extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            user: null
+        };
+    }
+
+    componentDidMount() {
+        this.fetchUserData();
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <LayoutHeader/>
+                <Row className="flex-grow-1 d-flex">
+                    <LayoutSidebar />
+                    <Col md={10} className="p-4">
+                        <Switch>
+                            <Route path="/verified/agency/create" component={CreateAgency}/>
+                            <Route path="/verified/agency" component={UpdateAgency}/>
+                            <Route path="/verified/agency-admin" component={AgencyAdminHome}/>
+                            <Route path="/verified/request-review" component={CreateReviewSolicitation}/>
+                        </Switch>
+                    </Col>
+                </Row>
+                <LayoutFooter user={this.state.user}/>
+            </BrowserRouter>
+        )
+    }
+
+    fetchUserData() {
+        fetch('/api/user')
+            .then((response) => {
+                if (!response.ok) throw new Error(response.status);
+                else return response.json();
+            })
+            .then(data => {
+                this.setState({
+                    user: data
+                });
+            })
+            .catch(err => console.error("Error:", err));
+    }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
