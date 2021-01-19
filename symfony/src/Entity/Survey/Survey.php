8127b15c36c27da26d660874d14983e21689d0a4
jackparnell <jack@starsol.co.uk>
2021-01-19 11:35:07 +0000

    Survey entity/repository

---
+++ symfony/src/Entity/Survey/Survey.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Entity\Survey;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\Survey\SurveyRepository")
+ * @Gedmo\SoftDeleteable(fieldName="deletedAt", timeAware=false, hardDelete=false)
+ */
+class Survey
+{
+    use SoftDeleteableEntity;
+    use TimestampableEntity;
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=false)
+     */
+    private string $title;
+
+    /**
+     * @ORM\Column(type="text", length=65535, nullable=true)
+     */
+    private ?string $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private string $slug;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=false, options={"default": true})
+     */
+    private bool $published = true;
+
+    public function __toString(): string
+    {
+        return (string) $this->getTitle();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function isPublished(): bool
+    {
+        return $this->published;
+    }
+
+    public function setPublished(bool $published): self
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+}
