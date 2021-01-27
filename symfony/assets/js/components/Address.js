ffbf99742f13ba7fe41df548efff846c7a46416e
jackparnell <jack@starsol.co.uk>
2021-01-27 22:34:09 +0000

    void(0)

--- symfony/assets/js/components/Address.js
+++ symfony/assets/js/components/Address.js
@@ -5,7 +5,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 const Address = (props) => {
     return (
         <div className="address">
-            <a href="javascript:void(0)" onClick={() => props.handleClick(props.addressLine1)}>
+            <a href={void(0)} onClick={() => props.handleClick(props.addressLine1)}>
                 <FontAwesomeIcon icon={faHome} />{' '}
                 {[props.addressLine1, props.addressLine2, props.addressLine3, props.city, props.postcode].filter(function (el) {return el.length > 0;}).join(', ')}
             </a>
