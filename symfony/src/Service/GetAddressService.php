6e30c96908d49be70c879bef3c2ac03e4adecdf2
jackparnell <jack@starsol.co.uk>
2021-01-27 10:56:55 +0000

    GetAddressService::find

--- symfony/src/Service/GetAddressService.php
+++ symfony/src/Service/GetAddressService.php
@@ -25,6 +25,7 @@ class GetAddressService
     {
         $uri = 'https://api.getAddress.io/autocomplete/'.$term.'?api-key='.$this->apiKey.'&top=10';
 
+        // TODO try/catch
         $response = $this->client->request('GET', $uri);
 
         $results = json_decode($response->getContent(), true)['suggestions'];
@@ -45,6 +46,7 @@ class GetAddressService
     {
         $uri = 'https://api.getAddress.io/get/'.$vendorId.'?api-key='.$this->apiKey;
 
+        // TODO try/catch
         $response = $this->client->request('GET', $uri);
 
         $result = json_decode($response->getContent(), true);
@@ -65,5 +67,44 @@ class GetAddressService
             $result['longitude'],
             $result['residential']
         );
+    }
+
+    /**
+     * @param string $inputPostcode
+     * @return VendorProperty[]
+     */
+    public function find(string $inputPostcode): iterable
+    {
+        $inputPostcode = preg_replace('/[^A-Za-z0-9 ]/', '', trim(strtolower($inputPostcode)));
+
+        $uri = 'https://api.getaddress.io/find/'.$inputPostcode.'?api-key='.$this->apiKey.'&expand=true';
+
+        // TODO try/catch
+        $response = $this->client->request('GET', $uri);
+
+        $result = json_decode($response->getContent(), true);
+
+        $postcode = $result['postcode'];
+        $latitude = $result['latitude'];
+        $longitude = $result['longitude'];
+
+        foreach ($result['addresses'] as $address) {
+            yield new VendorProperty(
+                'TODO', // TODO
+                $address['line_1'],
+                $address['line_2'],
+                $address['line_3'],
+                $address['line_4'],
+                $address['locality'],
+                $address['town_or_city'],
+                $address['county'],
+                $address['district'],
+                $address['country'],
+                $postcode,
+                $latitude,
+                $longitude,
+                $address['residential']
+            );
+        }
     }
 }
