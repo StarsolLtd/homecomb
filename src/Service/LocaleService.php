9cd4c25a21d40a62350f499ac6ff69e181c0f9ed
Jack Parnell <jack@starsol.co.uk>
2021-10-04 17:06:43 +0100

    Remove unused method

--- src/Service/LocaleService.php
+++ src/Service/LocaleService.php
@@ -2,14 +2,9 @@
 
 namespace App\Service;
 
-use App\Entity\Locale\Locale;
-use App\Entity\TenancyReview;
 use App\Factory\LocaleFactory;
-use App\Model\Agency\ReviewsSummary;
-use App\Model\Locale\AgencyReviewsSummary;
 use App\Model\Locale\LocaleSearchResults;
 use App\Repository\Locale\LocaleRepository;
-use LogicException;
 
 class LocaleService
 {
@@ -24,72 +19,5 @@ class LocaleService
         $results = $this->localeRepository->findBySearchQuery($query);
 
         return $this->localeFactory->createLocaleSearchResults($query, $results);
-    }
-
-    // TODO replace with version in factory
-    public function getAgencyReviewsSummary(Locale $locale): AgencyReviewsSummary
-    {
-        $agencies = [];
-        /** @var TenancyReview $tenancyReview */
-        foreach ($locale->getPublishedTenancyReviewsWithPublishedAgency() as $tenancyReview) {
-            $agency = $tenancyReview->getAgency();
-            if (null === $agency) {
-                throw new LogicException(sprintf('Review %s has no agency. ', $tenancyReview->getId()));
-            }
-
-            $slug = $agency->getSlug();
-            if (!isset($agencies[$slug])) {
-                $logoImage = $agency->getLogoImage();
-                $logoImageFilename = $logoImage ? $logoImage->getImage() : null;
-                $agencies[$slug] = [
-                    'name' => $agency->getName(),
-                    'logoImageFilename' => $logoImageFilename,
-                    '5' => 0,
-                    '4' => 0,
-                    '3' => 0,
-                    '2' => 0,
-                    '1' => 0,
-                    'score' => 0,
-                    'totalRated' => 0,
-                    'totalUnrated' => 0,
-                ];
-            }
-            if (null !== $tenancyReview->getAgencyStars()) {
-                $rating = $tenancyReview->getAgencyStars();
-                ++$agencies[$slug][(string) $rating];
-                ++$agencies[$slug]['totalRated'];
-                $agencies[$slug]['score'] += $rating;
-            } else {
-                ++$agencies[$slug]['totalUnrated'];
-            }
-        }
-
-        $agencyReviewSummaries = [];
-        $reviewsCount = 0;
-        foreach ($agencies as $agencySlug => $agency) {
-            $reviewsCount += $agency['totalRated'];
-            $meanRating = 0 < $agency['totalRated']
-                ? round($agency['score'] / $agency['totalRated'], 2)
-                : 0;
-
-            $agencyReviewSummaries[] = new ReviewsSummary(
-                $agencySlug,
-                $agency['name'],
-                $agency['logoImageFilename'],
-                $agency['5'],
-                $agency['4'],
-                $agency['3'],
-                $agency['2'],
-                $agency['1'],
-                $agency['totalRated'],
-                $agency['totalUnrated'],
-                $meanRating
-            );
-        }
-
-        // Order agencies by mean rating, descending
-        usort($agencyReviewSummaries, fn ($a, $b) => strcmp($b->getMeanRating(), $a->getMeanRating()));
-
-        return new AgencyReviewsSummary($agencyReviewSummaries, $reviewsCount, count($agencies));
     }
 }
--- tests/Unit/Service/LocaleServiceTest.php
+++ tests/Unit/Service/LocaleServiceTest.php
@@ -2,10 +2,6 @@
 
 namespace App\Tests\Unit\Service;
 
-use App\Entity\Agency;
-use App\Entity\Branch;
-use App\Entity\Locale\Locale;
-use App\Entity\TenancyReview;
 use App\Factory\LocaleFactory;
 use App\Model\Locale\LocaleSearchResults;
 use App\Repository\Locale\LocaleRepository;
@@ -52,52 +48,5 @@ final class LocaleServiceTest extends TestCase
         $output = $this->localeService->search('king');
 
         $this->assertEquals($output, $localeSearchResults->reveal());
-    }
-
-    /**
-     * @covers \App\Service\LocaleService::getAgencyReviewsSummary
-     */
-    public function testGetAgencyReviewsSummary(): void
-    {
-        $locale = new Locale();
-
-        $agency1 = (new Agency())->setName('Agency 1')->setPublished(true)->setSlug('ag1');
-        $branch1 = (new Branch())->setAgency($agency1)->setPublished(true);
-        $agency2 = (new Agency())->setName('Agency 2')->setPublished(true)->setSlug('ag2');
-        $branch2 = (new Branch())->setAgency($agency2)->setPublished(true);
-        $review1 = (new TenancyReview())->setBranch($branch1)->setPublished(true)->setAgencyStars(5);
-        $review2 = (new TenancyReview())->setBranch($branch1)->setPublished(true)->setAgencyStars(2);
-        $review3 = (new TenancyReview())->setBranch($branch2)->setPublished(true)->setAgencyStars(4);
-        $review4 = (new TenancyReview())->setBranch($branch2)->setPublished(false)->setAgencyStars(1);
-        $review5 = (new TenancyReview())->setBranch($branch2)->setPublished(true)->setAgencyStars(null);
-        $locale->addTenancyReview($review1)->addTenancyReview($review2)->addTenancyReview($review3)->addTenancyReview($review4)->addTenancyReview($review5);
-
-        $output = $this->localeService->getAgencyReviewsSummary($locale);
-
-        // Agencies should be ordered by mean rating descending, so the higher rated Agency 2 should be first
-        $this->assertEquals('Agency 2', $output->getAgencyReviewSummaries()[0]->getAgencyName());
-        $this->assertEquals('ag2', $output->getAgencyReviewSummaries()[0]->getAgencySlug());
-        $this->assertEquals(4, $output->getAgencyReviewSummaries()[0]->getMeanRating());
-        $this->assertEquals(1, $output->getAgencyReviewSummaries()[0]->getRatedCount());
-        $this->assertEquals(0, $output->getAgencyReviewSummaries()[0]->getFiveStarCount());
-        $this->assertEquals(1, $output->getAgencyReviewSummaries()[0]->getFourStarCount());
-        $this->assertEquals(0, $output->getAgencyReviewSummaries()[0]->getThreeStarCount());
-        $this->assertEquals(0, $output->getAgencyReviewSummaries()[0]->getTwoStarCount());
-        $this->assertEquals(0, $output->getAgencyReviewSummaries()[0]->getOneStarCount());
-        $this->assertEquals(1, $output->getAgencyReviewSummaries()[0]->getUnratedCount());
-
-        $this->assertEquals('Agency 1', $output->getAgencyReviewSummaries()[1]->getAgencyName());
-        $this->assertEquals('ag1', $output->getAgencyReviewSummaries()[1]->getAgencySlug());
-        $this->assertEquals(3.5, $output->getAgencyReviewSummaries()[1]->getMeanRating());
-        $this->assertEquals(2, $output->getAgencyReviewSummaries()[1]->getRatedCount());
-        $this->assertEquals(1, $output->getAgencyReviewSummaries()[1]->getFiveStarCount());
-        $this->assertEquals(0, $output->getAgencyReviewSummaries()[1]->getFourStarCount());
-        $this->assertEquals(0, $output->getAgencyReviewSummaries()[1]->getThreeStarCount());
-        $this->assertEquals(1, $output->getAgencyReviewSummaries()[1]->getTwoStarCount());
-        $this->assertEquals(0, $output->getAgencyReviewSummaries()[1]->getOneStarCount());
-        $this->assertEquals(0, $output->getAgencyReviewSummaries()[1]->getUnratedCount());
-
-        $this->assertEquals(3, $output->getTenancyReviewsCount());
-        $this->assertEquals(2, $output->getAgenciesCount());
     }
 }
