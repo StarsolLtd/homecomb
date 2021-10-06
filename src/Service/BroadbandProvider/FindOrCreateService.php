db7eddfc11b2c8cc87763038a9be00ace99f44c5
Jack Parnell <jack@starsol.co.uk>
2021-10-06 16:20:17 +0100

    FindOrCreateService

---
+++ src/Service/BroadbandProvider/FindOrCreateService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Service\BroadbandProvider;
+
+use App\Entity\BroadbandProvider;
+use App\Factory\BroadbandProviderFactory;
+use App\Repository\BroadbandProviderRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class FindOrCreateService
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private BroadbandProviderFactory $broadbandProviderFactory,
+        private BroadbandProviderRepository $broadbandProviderRepository
+    ) {
+    }
+
+    public function findOrCreate(string $name, ?string $countryCode): BroadbandProvider
+    {
+        $name = trim($name);
+        if (null !== $countryCode) {
+            $countryCode = trim($countryCode);
+        }
+
+        $broadbandProvider = $this->broadbandProviderRepository->findOneBy(
+            [
+                'name' => $name,
+                'countryCode' => $countryCode,
+            ]
+        );
+        if (null !== $broadbandProvider) {
+            return $broadbandProvider;
+        }
+
+        $broadbandProvider = $this->broadbandProviderFactory->createEntityFromNameAndCountryCode($name, $countryCode);
+
+        $this->entityManager->persist($broadbandProvider);
+        $this->entityManager->flush();
+
+        return $broadbandProvider;
+    }
+}
