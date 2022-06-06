0e271a6284a4595b1e73328354cca6c59894bd41
Jack Parnell <jackparnell@users.noreply.github.com>
2022-06-06 15:09:53 +0100

    TenancyReviewSolicitationTest additional tests (#119)

--- tests/Unit/Entity/TenancyReviewSolicitationTest.php
+++ tests/Unit/Entity/TenancyReviewSolicitationTest.php
@@ -2,11 +2,11 @@
 
 namespace App\Tests\Unit\Entity;
 
+use App\Entity\Branch;
+use App\Entity\Property;
+use App\Entity\TenancyReview;
 use App\Entity\TenancyReviewSolicitation;
 
-/**
- * @covers \App\Entity\TenancyReviewSolicitation
- */
 final class TenancyReviewSolicitationTest extends AbstractEntityTestCase
 {
     protected array $values = [
@@ -25,5 +25,29 @@ final class TenancyReviewSolicitationTest extends AbstractEntityTestCase
         assert($entity instanceof TenancyReviewSolicitation);
 
         return $entity;
+    }
+
+    public function testGetBranch1(): void
+    {
+        $entity = $this->getEntity();
+        $branch = new Branch();
+        $entity->setBranch($branch);
+        $this->assertEquals($branch, $entity->getBranch());
+    }
+
+    public function testGetProperty1(): void
+    {
+        $entity = $this->getEntity();
+        $property = new Property();
+        $entity->setProperty($property);
+        $this->assertEquals($property, $entity->getProperty());
+    }
+
+    public function testGetTenancyReview1(): void
+    {
+        $entity = $this->getEntity();
+        $tenancyReview = new TenancyReview();
+        $entity->setTenancyReview($tenancyReview);
+        $this->assertEquals($tenancyReview, $entity->getTenancyReview());
     }
 }
