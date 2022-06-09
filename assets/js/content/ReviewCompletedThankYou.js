a1a7c917e70e03dcf76cd44c472fdc549ae2eaf1
Jack Parnell <jack@starsol.co.uk>
2022-06-09 14:20:20 +0100

    Lint content

--- assets/js/content/ReviewCompletedThankYou.js
+++ assets/js/content/ReviewCompletedThankYou.js
@@ -1,20 +1,20 @@
-import React from "react";
-import {Alert} from "reactstrap";
+import React from 'react'
+import { Alert } from 'reactstrap'
 
 const ReviewCompletedThankYou = () => {
-    return (
-        <Alert color="success" className="p-4 text-center alert-success alert-dismissible fade show review-completed-thank-you">
-            <h2>Thank you!</h2>
+  return (
+    <Alert color="success" className="p-4 text-center alert-success alert-dismissible fade show review-completed-thank-you">
+      <h2>Thank you!</h2>
 
-            <p>
-                Your review was received successfully and will be checked by our moderation team shortly.
-            </p>
+      <p>
+        Your review was received successfully and will be checked by our moderation team shortly.
+      </p>
 
-            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </Alert>
-    );
+      <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </Alert>
+  )
 }
 
-export default ReviewCompletedThankYou;
+export default ReviewCompletedThankYou
--- assets/js/content/SurveyCompletedThankYou.js
+++ assets/js/content/SurveyCompletedThankYou.js
@@ -1,21 +1,21 @@
-import React from "react";
-import {Alert} from "reactstrap";
-import Constants from "../Constants";
+import React from 'react'
+import { Alert } from 'reactstrap'
+import Constants from '../Constants'
 
 const SurveyCompletedThankYou = () => {
-    return (
-        <Alert color="success" className="p-4 text-center alert-success alert-dismissible fade show survey-completed-thank-you">
-            <h2>Survey complete!</h2>
+  return (
+    <Alert color="success" className="p-4 text-center alert-success alert-dismissible fade show survey-completed-thank-you">
+      <h2>Survey complete!</h2>
 
-            <p>
-                Thank you! We have received your answers and will use them to make {Constants.SITE_NAME} even better!
-            </p>
+      <p>
+        Thank you! We have received your answers and will use them to make {Constants.SITE_NAME} even better!
+      </p>
 
-            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </Alert>
-    );
+      <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </Alert>
+  )
 }
 
-export default SurveyCompletedThankYou;
+export default SurveyCompletedThankYou
