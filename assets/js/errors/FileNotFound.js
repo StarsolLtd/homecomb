5a4bf752dc9015c5f8cf7548c8268164c87fc612
Jack Parnell <jack@starsol.co.uk>
2022-06-09 13:10:08 +0100

    Lint FileNotFound.js

--- assets/js/errors/FileNotFound.js
+++ assets/js/errors/FileNotFound.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import {Alert} from "reactstrap";
+import React from 'react'
+import { Alert } from 'reactstrap'
 
 const FileNotFound = () => {
-    return (
-        <Alert color="warning">
-            404 File Not Found. Sorry, whatever you were looking for we couldn't find it.
-        </Alert>
-    );
+  return (
+    <Alert color="warning">
+      404 File Not Found. Sorry, whatever you were looking for we couldn&apos;t find it.
+    </Alert>
+  )
 }
 
-export default FileNotFound;
+export default FileNotFound
