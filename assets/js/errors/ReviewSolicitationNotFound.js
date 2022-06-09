6e5a78cd555e07e0ed70a6b14cdaf14bdd6c62ab
Jack Parnell <jack@starsol.co.uk>
2022-06-09 13:11:49 +0100

    Lint error components

--- assets/js/errors/ReviewSolicitationNotFound.js
+++ assets/js/errors/ReviewSolicitationNotFound.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import {Alert} from "reactstrap";
+import React from 'react'
+import { Alert } from 'reactstrap'
 
 const ReviewSolicitationNotFound = () => {
-    return (
-        <Alert color="info">
-            Your review has been successfully received. Thank you!
-        </Alert>
-    );
+  return (
+    <Alert color="info">
+      Your review has been successfully received. Thank you!
+    </Alert>
+  )
 }
 
-export default ReviewSolicitationNotFound;
+export default ReviewSolicitationNotFound
