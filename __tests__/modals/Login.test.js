86fefd26089bc2195f8ab69f92de5080500d6be5
Jack Parnell <jack@starsol.co.uk>
2022-06-09 15:31:22 +0100

    Login modal updates

---
+++ __tests__/modals/Login.test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Login from "../../assets/js/modals/Login";
+
+it('renders without crashing', () => {
+  shallow(<Login />)
+})
--- assets/js/modals/Login.js
+++ assets/js/modals/Login.js
@@ -1,30 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
 import { HashLink as Link } from 'react-router-hash-link'
 import Constants from '../Constants'
+import PropTypes from 'prop-types'
 
-export default class Login extends Component {
-  closeModal = () => {
-    this.props.hideLoginModal()
-  }
-
-  render () {
-    return (
-      <Modal className="login-modal" isOpen={true} toggle={this.closeModal}>
-        <ModalHeader toggle={this.closeModal}>Log in</ModalHeader>
-        <ModalBody>
-          <a href="/connect/google">
-            <Button size="md" color="primary">
-              Log in with Google
-            </Button>
-          </a>
-        </ModalBody>
-        <ModalFooter>
-          <a href="/login">Log in with email</a>
-          | <Link to="/register#" onClick={this.closeModal}>Register with {Constants.SITE_NAME}</Link>
-        </ModalFooter>
-      </Modal>
-    )
-  }
+const Login = (props) => {
+  return (
+    <Modal className="login-modal" isOpen={true} toggle={props.hideLoginModal}>
+      <ModalHeader toggle={props.hideLoginModal}>Log in</ModalHeader>
+      <ModalBody>
+        <a href="/connect/google">
+          <Button size="md" color="primary">
+            Log in with Google
+          </Button>
+        </a>
+      </ModalBody>
+      <ModalFooter>
+        <a href="/login">Log in with email</a>
+        | <Link to="/register#" onClick={props.hideLoginModal}>Register with {Constants.SITE_NAME}</Link>
+      </ModalFooter>
+    </Modal>
+  )
 }
+
+Login.propTypes = {
+  hideLoginModal: PropTypes.func
+}
+
+export default Login
