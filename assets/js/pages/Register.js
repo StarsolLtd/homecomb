ca8c7dd30b5c02dc2f620abcb66e6f52d6d241df
Jack Parnell <jack@starsol.co.uk>
2022-06-09 13:29:40 +0100

    Lint some pages

--- assets/js/pages/Register.js
+++ assets/js/pages/Register.js
@@ -1,37 +1,36 @@
-import React, {useEffect} from 'react';
-import {Container, Col, Row, Breadcrumb, BreadcrumbItem} from 'reactstrap';
-import Constants from "../Constants";
-import RegisterForm from "../components/RegisterForm";
-import { HashLink as Link } from "react-router-hash-link";
+import React, { useEffect } from 'react'
+import { Container, Col, Row, Breadcrumb, BreadcrumbItem } from 'reactstrap'
+import Constants from '../Constants'
+import RegisterForm from '../components/RegisterForm'
+import { HashLink as Link } from 'react-router-hash-link'
 
 const Register = (props) => {
+  useEffect(() => {
+    document.title = Constants.SITE_NAME + ' | Register'
+  })
 
-    useEffect(() => {
-        document.title = Constants.SITE_NAME + ' | Register';
-    });
+  return (
+    <Container>
+      <Row className="desktop-only">
+        <Breadcrumb className="w-100">
+          <BreadcrumbItem><Link to="/#">{Constants.SITE_NAME}</Link></BreadcrumbItem>
+          <BreadcrumbItem className="active">Register with {Constants.SITE_NAME}</BreadcrumbItem>
+        </Breadcrumb>
+      </Row>
 
-    return (
-        <Container>
-            <Row className="desktop-only">
-                <Breadcrumb className="w-100">
-                    <BreadcrumbItem><Link to="/#">{Constants.SITE_NAME}</Link></BreadcrumbItem>
-                    <BreadcrumbItem className="active">Register with {Constants.SITE_NAME}</BreadcrumbItem>
-                </Breadcrumb>
-            </Row>
+      <Row>
+        <Col md={12} className="bg-white rounded shadow-sm p-4 mb-4">
+          <p>
+            To create an account with us, please complete the form below:
+          </p>
 
-            <Row>
-                <Col md={12} className="bg-white rounded shadow-sm p-4 mb-4">
-                    <p>
-                        To create an account with us, please complete the form below:
-                    </p>
+          <hr />
 
-                    <hr />
-
-                    <RegisterForm {...props} />
-                </Col>
-            </Row>
-        </Container>
-    );
+          <RegisterForm {...props} />
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
-export default Register;
+export default Register
--- assets/js/pages/Survey.js
+++ assets/js/pages/Survey.js
@@ -1,105 +1,104 @@
-import React, {Fragment} from 'react';
-import {Col, Container, Progress, Row} from 'reactstrap';
-import DataLoader from "../components/DataLoader";
-import Constants from "../Constants";
-import Question from "../components/Question";
-import SurveyCompletedThankYou from "../content/SurveyCompletedThankYou";
+import React, { Fragment } from 'react'
+import { Col, Container, Progress, Row } from 'reactstrap'
+import DataLoader from '../components/DataLoader'
+import Constants from '../Constants'
+import Question from '../components/Question'
+import SurveyCompletedThankYou from '../content/SurveyCompletedThankYou'
 
 export default class Survey extends React.Component {
+  state = {
+    title: '',
+    description: '',
+    currentQuestion: 1,
+    loaded: false
+  }
 
-    state = {
-        title: '',
-        description: '',
-        currentQuestion: 1,
-        loaded: false,
-    };
+  render () {
+    return (
+      <Container>
+        <DataLoader
+          url={'/api/s/' + this.props.match.params.slug}
+          loadComponentData={this.loadData}
+        />
+        {this.state.loaded &&
+          <div>
+            <Row>
+              <Col md="12" className="page-title">
+                <h1>
+                  {this.state.title}
+                </h1>
+                <p>
+                  {this.state.description}
+                </p>
+              </Col>
+            </Row>
+            <div className="bg-white rounded shadow-sm p-4 mb-4">
+              <Row>
+                <Col md="12">
+                  {this.state.questions.map(
+                    ({ id, type, content, help, highMeaning, lowMeaning, sortOrder, choices }) => (
+                      <Question
+                        {...this.props}
+                        key={id}
+                        questionId={id}
+                        type={type}
+                        content={content}
+                        choices={choices}
+                        help={help}
+                        highMeaning={highMeaning}
+                        lowMeaning={lowMeaning}
+                        sortOrder={sortOrder}
+                        totalQuestions={this.state.questions.length}
+                        visible={sortOrder === this.state.currentQuestion}
+                        back={this.back}
+                        forward={this.forward}
+                      />
+                    )
+                  )}
+                  {this.state.currentQuestion > this.state.questions.length &&
+                    <Fragment>
+                      <p>
+                        All questions answered
+                      </p>
+                      <Progress
+                        min={1}
+                        max={this.state.questions.length + 1}
+                        value={this.state.questions.length + 1}
+                        color="primary"
+                      />
+                      <hr />
+                      <SurveyCompletedThankYou />
+                    </Fragment>
+                  }
+                </Col>
+              </Row>
+            </div>
+          </div>
+        }
+      </Container>
+    )
+  }
 
-    render() {
-        return (
-            <Container>
-                <DataLoader
-                    url={'/api/s/' + this.props.match.params.slug}
-                    loadComponentData={this.loadData}
-                />
-                {this.state.loaded &&
-                    <div>
-                        <Row>
-                            <Col md="12" className="page-title">
-                                <h1>
-                                    {this.state.title}
-                                </h1>
-                                <p>
-                                    {this.state.description}
-                                </p>
-                            </Col>
-                        </Row>
-                        <div className="bg-white rounded shadow-sm p-4 mb-4">
-                            <Row>
-                                <Col md="12">
-                                    {this.state.questions.map(
-                                        ({ id, type, content, help, highMeaning, lowMeaning, sortOrder, choices }) => (
-                                            <Question
-                                                {...this.props}
-                                                key={id}
-                                                questionId={id}
-                                                type={type}
-                                                content={content}
-                                                choices={choices}
-                                                help={help}
-                                                highMeaning={highMeaning}
-                                                lowMeaning={lowMeaning}
-                                                sortOrder={sortOrder}
-                                                totalQuestions={this.state.questions.length}
-                                                visible={sortOrder === this.state.currentQuestion}
-                                                back={this.back}
-                                                forward={this.forward}
-                                            />
-                                        )
-                                    )}
-                                    {this.state.currentQuestion > this.state.questions.length &&
-                                        <Fragment>
-                                            <p>
-                                                All questions answered
-                                            </p>
-                                            <Progress
-                                                min={1}
-                                                max={this.state.questions.length + 1}
-                                                value={this.state.questions.length + 1}
-                                                color="primary"
-                                            />
-                                            <hr />
-                                            <SurveyCompletedThankYou />
-                                        </Fragment>
-                                    }
-                                </Col>
-                            </Row>
-                        </div>
-                    </div>
-                }
-            </Container>
-        );
-    }
+  loadData = (data) => {
+    this.setState({
+      title: data.title,
+      description: data.description,
+      questions: data.questions,
+      loaded: true
+    })
 
-    loadData = (data) => {
-        this.setState({
-            title: data.title,
-            description: data.description,
-            questions: data.questions,
-            loaded: true,
-        });
+    document.title = this.state.title + ' | ' + Constants.SITE_NAME
+  }
 
-        document.title = this.state.title + ' | ' + Constants.SITE_NAME;
-    }
+  back = (number) => {
+    this.setState({
+      currentQuestion: number - 1
+    })
+  }
 
-    back = (number) => {
-        this.setState({
-            currentQuestion: number - 1,
-        });
-    }
-
-    forward = (number) => {
-        this.setState({
-            currentQuestion: number + 1,
-        });
-    }
+  forward = (number) => {
+    this.setState({
+      currentQuestion: number + 1
+    })
+  }
 }
