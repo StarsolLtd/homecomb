88ef8c048ce759b4124ab8d393b7de5cafee293a
Jack Parnell <jack@starsol.co.uk>
2022-06-13 22:29:30 +0100

    CreateBranch component updates

---
+++ __tests__/pages/AgencyAdmin/CreateBranch.test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import CreateBranch from '../../../assets/js/pages/AgencyAdmin/CreateBranch'
+
+it('renders without crashing', () => {
+  shallow(<CreateBranch />)
+})
--- assets/js/pages/AgencyAdmin/CreateBranch.js
+++ assets/js/pages/AgencyAdmin/CreateBranch.js
@@ -1,83 +1,86 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import PropTypes from 'prop-types'
+
 import { Label, Button, FormText, Container } from 'reactstrap'
 import { AvForm, AvGroup, AvInput, AvFeedback } from 'availity-reactstrap-validation'
 
-export default class CreateBranch extends React.Component {
-  state = {
-    branchName: '',
-    telephone: '',
-    email: ''
-  }
+const CreateBranch = (props) => {
+  const [branchName, setBranchName] = useState('')
+  const [telephone, setTelephone] = useState('')
+  const [email, setEmail] = useState('')
 
-  constructor (props) {
-    super(props)
-    this.submit = this.props.submit
-    this.submit = this.submit.bind(this)
-  }
+  const createBranchForm = useRef(null)
 
-  handleChange = (event) => {
+  const handleChange = (event) => {
     const target = event.target
     const value = target.type === 'checkbox' ? target.checked : target.value
-    const name = target.name
 
-    this.setState({
-      [name]: value
-    })
-  }
-
-  render () {
-    return (
-      <Container>
-        <h1>Add a branch</h1>
-        <p>
-          Please complete the form below to add a new branch to your agency.
-        </p>
-        <AvForm onValidSubmit={this.handleValidSubmit} ref={c => (this.form = c)}>
-          <AvGroup>
-            <Label for="branchName">Branch name</Label>
-            <AvInput name="branchName" placeholder="Branch location. Example: Cambridge" required onChange={this.handleChange} />
-            <AvFeedback>Please enter your name.</AvFeedback>
-            <FormText>
-              Please enter the location of your branch. Please use the city/town/locality name. Examples: <i>Cambridge</i> or <i>Shoreditch</i>.
-            </FormText>
-          </AvGroup>
-          <AvGroup>
-            <Label for="telephone">Telephone</Label>
-            <AvInput name="telephone" placeholder="Branch telephone number" onChange={this.handleChange} />
-            <FormText>
-              Optional. The telephone number of this branch. We will publish this.
-            </FormText>
-          </AvGroup>
-          <AvGroup>
-            <Label for="email">Email Address</Label>
-            <AvInput name="email" placeholder="Example: branch@youragency.com" onChange={this.handleChange} />
-            <FormText>
-              Optional. The email address of this branch. We will publish this.
-            </FormText>
-          </AvGroup>
-          <Button color="primary">
-            Add your branch
-          </Button>
-        </AvForm>
-      </Container>
-    )
-  }
-
-  handleValidSubmit = () => {
-    const payload = {
-      branchName: this.state.branchName,
-      externalUrl: this.state.externalUrl,
-      postcode: this.state.postcode
+    switch (target.name) {
+      case 'branchName':
+        setBranchName(value)
+        break
+      case 'telephone':
+        setTelephone(value)
+        break
+      case 'email':
+        setEmail(value)
+        break
     }
-    this.submit(
+  }
+
+  const handleValidSubmit = () => {
+    const payload = {
+      branchName,
+      telephone,
+      email
+    }
+    props.submit(
       payload,
       '/api/verified/branch',
       'POST'
     )
-    this.clearForm()
+    createBranchForm.current.reset()
   }
 
-  clearForm = () => {
-    this.form && this.form.reset()
-  }
+  return (
+    <Container>
+      <h1>Add a branch</h1>
+      <p>
+        Please complete the form below to add a new branch to your agency.
+      </p>
+      <AvForm onValidSubmit={handleValidSubmit} ref={createBranchForm}>
+        <AvGroup>
+          <Label for="branchName">Branch name</Label>
+          <AvInput name="branchName" placeholder="Branch location. Example: Cambridge" required onChange={handleChange} />
+          <AvFeedback>Please enter your name.</AvFeedback>
+          <FormText>
+            Please enter the location of your branch. Please use the city/town/locality name. Examples: <i>Cambridge</i> or <i>Shoreditch</i>.
+          </FormText>
+        </AvGroup>
+        <AvGroup>
+          <Label for="telephone">Telephone</Label>
+          <AvInput name="telephone" placeholder="Branch telephone number" onChange={handleChange} />
+          <FormText>
+            Optional. The telephone number of this branch. We will publish this.
+          </FormText>
+        </AvGroup>
+        <AvGroup>
+          <Label for="email">Email Address</Label>
+          <AvInput name="email" placeholder="Example: branch@youragency.com" onChange={handleChange} />
+          <FormText>
+            Optional. The email address of this branch. We will publish this.
+          </FormText>
+        </AvGroup>
+        <Button color="primary">
+          Add your branch
+        </Button>
+      </AvForm>
+    </Container>
+  )
 }
+
+CreateBranch.propTypes = {
+  submit: PropTypes.func
+}
+
+export default CreateBranch
