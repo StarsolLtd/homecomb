d938ed7c25bbd73ca6762bbbfebfe99e29dcfa6f
Jack Parnell <jack@starsol.co.uk>
2022-06-13 13:24:08 +0100

    Scale component test

--- assets/js/components/Scale.js
+++ assets/js/components/Scale.js
@@ -6,6 +6,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '../../styles/question.scss'
 
 export default class Scale extends React.Component {
+  state = {
+    rating: this.props.rating ?? 0
+  }
+
   handleRatingChange = (value) => {
     this.setState({ rating: value })
   }
@@ -16,7 +20,7 @@ export default class Scale extends React.Component {
         <span className="meaning low-meaning">{this.props.lowMeaning}</span>
         <Rating
           onChange={this.handleRatingChange}
-          initialRating={this.props.rating}
+          initialRating={this.state.rating}
           emptySymbol={
             <span className="text-rating-unchecked rating-icon"><FontAwesomeIcon icon={faStar}/></span>
           }
