b5f4ba445caf4b922e65b07c0cacd166b3cade9b
Jack Parnell <jackparnell@users.noreply.github.com>
2022-06-13 13:47:26 +0100

    Scale component updates (#132)
    * Scale component test
    
    * Scale component test
    
    * Scale component updates

--- assets/js/components/Scale.js
+++ assets/js/components/Scale.js
@@ -1,31 +1,44 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Rating from 'react-rating'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import '../../styles/question.scss'
 
-export default class Scale extends React.Component {
-  handleRatingChange = (value) => {
-    this.setState({ rating: value })
-  }
-
-  render () {
-    return (
-      <div className="scale-5">
-        <span className="meaning low-meaning">{this.props.lowMeaning}</span>
-        <Rating
-          onChange={this.handleRatingChange}
-          initialRating={this.props.rating}
-          emptySymbol={
-            <span className="text-rating-unchecked rating-icon"><FontAwesomeIcon icon={faStar}/></span>
-          }
-          fullSymbol={
-            <span className="text-rating rating-icon"><FontAwesomeIcon icon={faStar}/></span>
-          }
-        />
-        <span className="meaning high-meaning">{this.props.highMeaning}</span>
-      </div>
-    )
-  }
+const Scale = (props) => {
+  return (
+    <div className="scale-5">
+      <span className="meaning low-meaning">{props.lowMeaning}</span>
+      <Rating
+        onChange={props.handleRatingChange}
+        initialRating={props.rating}
+        emptySymbol={
+          <span className="text-rating-unchecked rating-icon"><FontAwesomeIcon icon={faStar}/></span>
+        }
+        fullSymbol={
+          <span className="text-rating rating-icon"><FontAwesomeIcon icon={faStar}/></span>
+        }
+        stop={props.max}
+      />
+      <span className="meaning high-meaning">{props.highMeaning}</span>
+    </div>
+  )
 }
+
+Scale.propTypes = {
+  lowMeaning: PropTypes.string,
+  highMeaning: PropTypes.string,
+  rating: PropTypes.number,
+  max: PropTypes.number,
+  handleRatingChange: PropTypes.func
+}
+
+Scale.defaultProps = {
+  lowMeaning: '',
+  highMeaning: 0,
+  rating: 0,
+  max: 5
+}
+
+export default Scale
