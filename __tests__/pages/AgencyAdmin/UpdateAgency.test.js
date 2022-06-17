2c174d3901974720d77ac5d53f28623ed9dfc2d0
Jack Parnell <jack@starsol.co.uk>
2022-06-17 10:27:28 +0100

    UpdateAgency component changes

---
+++ __tests__/pages/AgencyAdmin/UpdateAgency.test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import UpdateAgency from '../../../assets/js/pages/AgencyAdmin/UpdateAgency'
+
+it('renders without crashing', () => {
+  shallow(<UpdateAgency />)
+})
