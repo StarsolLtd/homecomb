53b7db80175245f019db4d95e2309e0d0abd4ff7
Jack Parnell <jackparnell@users.noreply.github.com>
2022-06-17 10:28:48 +0100

    UpdateAgency component changes (#140)

--- assets/js/pages/AgencyAdmin/UpdateAgency.js
+++ assets/js/pages/AgencyAdmin/UpdateAgency.js
@@ -1,94 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import { Label, FormText, Button, Container } from 'reactstrap'
 import DataLoader from '../../components/DataLoader'
 import { AvForm, AvGroup, AvInput } from 'availity-reactstrap-validation'
 
-export default class UpdateAgency extends React.Component {
-  state = {
-    slug: '',
-    name: '',
-    externalUrl: '',
-    postcode: '',
-    loaded: false
+const UpdateAgency = (props) => {
+  const [slug, setSlug] = useState('')
+  const [name, setName] = useState('')
+  const [externalUrl, setExternalUrl] = useState('')
+  const [postcode, setPostcode] = useState('')
+  const [loaded, setLoaded] = useState(false)
+
+  const loadData = (data) => {
+    setSlug(data.slug)
+    setName(data.name)
+    setExternalUrl(data.externalUrl)
+    setPostcode(data.postcode)
+    setLoaded(true)
   }
 
-  constructor (props) {
-    super(props)
-    this.submit = this.props.submit
-    this.submit = this.submit.bind(this)
-  }
-
-  handleChange = (event) => {
+  const handleChange = (event) => {
     const target = event.target
     const value = target.type === 'checkbox' ? target.checked : target.value
-    const name = target.name
 
-    this.setState({
-      [name]: value
-    })
-  }
-
-  render () {
-    return (
-      <Container>
-        <DataLoader
-          url='/api/verified/agency'
-          loadComponentData={this.loadData}
-        />
-        {this.state.loaded &&
-          <>
-            <h1>Update {this.state.name}</h1>
-            <AvForm id="update-agency-form" onValidSubmit={this.handleValidSubmit}>
-              <AvGroup>
-                <Label for="agencyName">Agency name</Label>
-                <AvInput name="agencyName" value={this.state.name} disabled />
-                <FormText>
-                  If you would like to change your agency name, please contact us.
-                </FormText>
-              </AvGroup>
-              <AvGroup>
-                <Label for="externalUrl">Website URL</Label>
-                <AvInput name="externalUrl" type="url" value={this.state.externalUrl} placeholder="http://yoursite.com" onChange={this.handleChange} />
-                <FormText>
-                  Optional. If your agency has a website, enter its URL here. Example: http://www.cambridgelettings.com/
-                </FormText>
-              </AvGroup>
-              <AvGroup>
-                <Label for="postcode">Postcode</Label>
-                <AvInput name="postcode" value={this.state.postcode} onChange={this.handleChange} />
-                <FormText>
-                  Optional. Please enter the postcode of your agency&apos;s primary office.
-                </FormText>
-              </AvGroup>
-              <Button color="primary">
-                Update your agency details
-              </Button>
-            </AvForm>
-          </>
-        }
-      </Container>
-    )
-  }
-
-  loadData = (data) => {
-    this.setState({
-      slug: data.slug,
-      name: data.name,
-      externalUrl: data.externalUrl,
-      postcode: data.postcode,
-      loaded: true
-    })
-  }
-
-  handleValidSubmit = () => {
-    const payload = {
-      externalUrl: this.state.externalUrl,
-      postcode: this.state.postcode
+    switch (target.name) {
+      case 'name':
+        setName(value)
+        break
+      case 'externalUrl':
+        setExternalUrl(value)
+        break
+      case 'postcode':
+        setPostcode(value)
+        break
     }
-    this.submit(
+  }
+
+  const handleValidSubmit = () => {
+    const payload = {
+      externalUrl,
+      postcode
+    }
+    props.submit(
       payload,
-      '/api/verified/agency/' + this.state.slug,
+      '/api/verified/agency/' + slug,
       'PUT'
     )
   }
+
+  return (
+    <Container>
+      <DataLoader url='/api/verified/agency' loadComponentData={loadData}/>
+      {loaded &&
+        <>
+          <h1>Update {name}</h1>
+          <AvForm id="update-agency-form" onValidSubmit={handleValidSubmit}>
+            <AvGroup>
+              <Label for="agencyName">Agency name</Label>
+              <AvInput name="agencyName" value={name} disabled />
+              <FormText>
+                If you would like to change your agency name, please contact us.
+              </FormText>
+            </AvGroup>
+            <AvGroup>
+              <Label for="externalUrl">Website URL</Label>
+              <AvInput name="externalUrl" type="url" value={externalUrl} placeholder="http://yoursite.com" onChange={handleChange} />
+              <FormText>
+                Optional. If your agency has a website, enter its URL here. Example: http://www.cambridgelettings.com/
+              </FormText>
+            </AvGroup>
+            <AvGroup>
+              <Label for="postcode">Postcode</Label>
+              <AvInput name="postcode" value={postcode} onChange={handleChange} />
+              <FormText>
+                Optional. Please enter the postcode of your agency&apos;s primary office.
+              </FormText>
+            </AvGroup>
+            <Button color="primary">
+              Update your agency details
+            </Button>
+          </AvForm>
+        </>
+      }
+    </Container>
+  )
 }
+
+UpdateAgency.propTypes = {
+  submit: PropTypes.func
+}
+
+export default UpdateAgency
