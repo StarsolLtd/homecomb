9189e18bb7cba27ed0fafa4972deb614a34c44c8
Jack Parnell <jackparnell@users.noreply.github.com>
2022-06-24 17:18:24 +0100

    LoginOrRegister component changes (#144)

--- assets/js/modals/LoginOrRegister.js
+++ assets/js/modals/LoginOrRegister.js
@@ -1,34 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
 import Constants from '../Constants'
 import { HashLink as Link } from 'react-router-hash-link'
+import PropTypes from 'prop-types'
 
-export default class LoginOrRegister extends React.Component {
-  state = {
-    showModal: this.props.showModal
+const LoginOrRegister = (props) => {
+  const [modalVisible, setModalVisible] = useState(props.showModal)
+
+  const toggleModal = () => {
+    setModalVisible(!modalVisible)
+    props.hideLoginModal()
   }
 
-  toggleModal = () => {
-    this.setState({
-      showModal: !this.state.showModal
-    })
-    this.props.hideLoginModal()
-  }
-
-  render () {
-    return (
-      <Modal className="login-modal" isOpen={this.state.showModal} toggle={this.toggleModal}>
-        <ModalHeader toggle={this.toggleModal}>Please log in or register</ModalHeader>
-        <ModalBody>
-          To use this functionality, you need to be logged in to {Constants.SITE_NAME}.
-        </ModalBody>
-        <ModalFooter>
-          <a href="/login"><Button color="primary" className="log-in-button">Log in</Button></a>
-          <Link to="/register#"><Button color="primary" className="register-button">Register</Button></Link>
-          <Button color="secondary" className="close-modal-button" onClick={this.toggleModal}>Never mind</Button>
-        </ModalFooter>
-      </Modal>
-    )
-  }
+  return (
+    <Modal className="login-modal" isOpen={modalVisible} toggle={toggleModal}>
+      <ModalHeader toggle={toggleModal}>Please log in or register</ModalHeader>
+      <ModalBody>
+        To use this functionality, you need to be logged in to {Constants.SITE_NAME}.
+      </ModalBody>
+      <ModalFooter>
+        <a href="/login"><Button color="primary" className="log-in-button">Log in</Button></a>
+        <Link to="/register#"><Button color="primary" className="register-button">Register</Button></Link>
+        <Button color="secondary" className="close-modal-button" onClick={toggleModal}>Never mind</Button>
+      </ModalFooter>
+    </Modal>
+  )
 }
+
+LoginOrRegister.propTypes = {
+  showModal: PropTypes.bool,
+  hideLoginModal: PropTypes.func
+}
+
+export default LoginOrRegister
