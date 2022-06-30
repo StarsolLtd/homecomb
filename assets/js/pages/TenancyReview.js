6f5b5a056a4f52868485c4246f7e63cdaa0e7911
Jack Parnell <jackparnell@users.noreply.github.com>
2022-06-30 13:49:08 +0100

    TenancyReview component changes (#145)

--- assets/js/pages/TenancyReview.js
+++ assets/js/pages/TenancyReview.js
@@ -1,54 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Col, Row, Breadcrumb, BreadcrumbItem } from 'reactstrap'
 import ReviewTenancyForm from '../components/ReviewTenancyForm'
 import Constants from '../Constants'
 import ReviewCompletedThankYou from '../content/ReviewCompletedThankYou'
 import { HashLink as Link } from 'react-router-hash-link'
 
-export default class TenancyReview extends React.Component {
-  state = {
-    completedThankYou: false
-  }
+const TenancyReview = (props) => {
+  const [showCompletedThankYou, setShowCompletedThankYou] = useState(false)
 
-  componentDidMount () {
+  useEffect(() => {
     document.title = Constants.SITE_NAME + ' | Review Tenancy'
+  }, [])
+
+  const completedThankYou = () => {
+    setShowCompletedThankYou(true)
   }
 
-  completedThankYou = () => {
-    this.setState({ completedThankYou: true })
+  if (showCompletedThankYou) {
+    return <ReviewCompletedThankYou />
   }
 
-  render () {
-    if (this.state.completedThankYou) {
-      return <ReviewCompletedThankYou />
-    }
+  return (
+    <Container>
+      <Row className="desktop-only">
+        <Breadcrumb className="w-100">
+          <BreadcrumbItem><Link to="/#">{Constants.SITE_NAME}</Link></BreadcrumbItem>
+          <BreadcrumbItem className="active">Review your tenancy</BreadcrumbItem>
+        </Breadcrumb>
+      </Row>
 
-    return (
-      <Container>
-        <Row className="desktop-only">
-          <Breadcrumb className="w-100">
-            <BreadcrumbItem><Link to="/#">{Constants.SITE_NAME}</Link></BreadcrumbItem>
-            <BreadcrumbItem className="active">Review your tenancy</BreadcrumbItem>
-          </Breadcrumb>
-        </Row>
+      <Row>
+        <Col md={12} className="bg-white rounded shadow-sm p-4 mb-4">
+          <p>
+            Please complete the form below to submit your review:
+          </p>
 
-        <Row>
-          <Col md={12} className="bg-white rounded shadow-sm p-4 mb-4">
-            <p>
-              Please complete the form below to submit your review:
-            </p>
+          <hr />
 
-            <hr />
-
-            <ReviewTenancyForm
-              fixedBranch={false}
-              fixedProperty={false}
-              {...this.props}
-              completedThankYou={this.completedThankYou}
-            />
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+          <ReviewTenancyForm
+            fixedBranch={false}
+            fixedProperty={false}
+            {...props}
+            completedThankYou={completedThankYou}
+          />
+        </Col>
+      </Row>
+    </Container>
+  )
 }
+
+export default TenancyReview
