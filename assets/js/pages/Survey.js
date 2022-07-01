c07bf66e640d27ea8cd0c68d9dcea20b3a11102f
Jack Parnell <jackparnell@users.noreply.github.com>
2022-07-01 16:01:16 +0100

    Survey component changes (#149)

--- assets/js/pages/Survey.js
+++ assets/js/pages/Survey.js
@@ -1,104 +1,106 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Col, Container, Progress, Row } from 'reactstrap'
 import DataLoader from '../components/DataLoader'
 import Constants from '../Constants'
 import Question from '../components/Question'
 import SurveyCompletedThankYou from '../content/SurveyCompletedThankYou'
+import PropTypes from 'prop-types'
 
-export default class Survey extends React.Component {
-  state = {
-    title: '',
-    description: '',
-    currentQuestion: 1,
-    loaded: false
+const Survey = (props) => {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [currentQuestion, setCurrentQuestion] = useState(1)
+  const [questions, setQuestions] = useState([])
+  const [loaded, setLoaded] = useState(false)
+
+  const loadData = (data) => {
+    setTitle(data.title)
+    setDescription(data.description)
+    setQuestions(data.questions)
+    setLoaded(true)
+
+    document.title = data.title + ' | ' + Constants.SITE_NAME
   }
 
-  render () {
-    return (
-      <Container>
-        <DataLoader
-          url={'/api/s/' + this.props.match.params.slug}
-          loadComponentData={this.loadData}
-        />
-        {this.state.loaded &&
-          <div>
+  const back = (number) => {
+    setCurrentQuestion(number - 1)
+  }
+
+  const forward = (number) => {
+    setCurrentQuestion(number + 1)
+  }
+
+  return (
+    <Container>
+      <DataLoader
+        url={'/api/s/' + props.match.params.slug}
+        loadComponentData={loadData}
+      />
+      {loaded &&
+        <div>
+          <Row>
+            <Col md="12" className="page-title">
+              <h1>
+                {title}
+              </h1>
+              <p>
+                {description}
+              </p>
+            </Col>
+          </Row>
+          <div className="bg-white rounded shadow-sm p-4 mb-4">
             <Row>
-              <Col md="12" className="page-title">
-                <h1>
-                  {this.state.title}
-                </h1>
-                <p>
-                  {this.state.description}
-                </p>
+              <Col md="12">
+                {questions.map(
+                  ({ id, type, content, help, highMeaning, lowMeaning, sortOrder, choices }) => (
+                    <Question
+                      {...props}
+                      key={id}
+                      questionId={id}
+                      type={type}
+                      content={content}
+                      choices={choices}
+                      help={help}
+                      highMeaning={highMeaning}
+                      lowMeaning={lowMeaning}
+                      sortOrder={sortOrder}
+                      totalQuestions={questions.length}
+                      visible={sortOrder === currentQuestion}
+                      back={back}
+                      forward={forward}
+                    />
+                  )
+                )}
+                {currentQuestion > questions.length &&
+                  <>
+                    <p>
+                      All questions answered
+                    </p>
+                    <Progress
+                      min={1}
+                      max={questions.length + 1}
+                      value={questions.length + 1}
+                      color="primary"
+                    />
+                    <hr />
+                    <SurveyCompletedThankYou />
+                  </>
+                }
               </Col>
             </Row>
-            <div className="bg-white rounded shadow-sm p-4 mb-4">
-              <Row>
-                <Col md="12">
-                  {this.state.questions.map(
-                    ({ id, type, content, help, highMeaning, lowMeaning, sortOrder, choices }) => (
-                      <Question
-                        {...this.props}
-                        key={id}
-                        questionId={id}
-                        type={type}
-                        content={content}
-                        choices={choices}
-                        help={help}
-                        highMeaning={highMeaning}
-                        lowMeaning={lowMeaning}
-                        sortOrder={sortOrder}
-                        totalQuestions={this.state.questions.length}
-                        visible={sortOrder === this.state.currentQuestion}
-                        back={this.back}
-                        forward={this.forward}
-                      />
-                    )
-                  )}
-                  {this.state.currentQuestion > this.state.questions.length &&
-                    <>
-                      <p>
-                        All questions answered
-                      </p>
-                      <Progress
-                        min={1}
-                        max={this.state.questions.length + 1}
-                        value={this.state.questions.length + 1}
-                        color="primary"
-                      />
-                      <hr />
-                      <SurveyCompletedThankYou />
-                    </>
-                  }
-                </Col>
-              </Row>
-            </div>
           </div>
-        }
-      </Container>
-    )
-  }
-
-  loadData = (data) => {
-    this.setState({
-      title: data.title,
-      description: data.description,
-      questions: data.questions,
-      loaded: true
-    })
-
-    document.title = this.state.title + ' | ' + Constants.SITE_NAME
-  }
-
-  back = (number) => {
-    this.setState({
-      currentQuestion: number - 1
-    })
-  }
-
-  forward = (number) => {
-    this.setState({
-      currentQuestion: number + 1
-    })
-  }
+        </div>
+      }
+    </Container>
+  )
 }
+
+Survey.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      slug: PropTypes.string
+    })
+  })
+}
+
+export default Survey
